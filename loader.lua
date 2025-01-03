4729fabb0a6c4b9b97d9526e5921e20c1d8c2c1c
144hert <badussian395@gmail.com>
2025-01-03 21:58:12 +0000

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -98,4 +98,23 @@ local alien = --[[
 ....................................................................................................
 ]]--
 
+local Lighting     = game:GetService("Lighting")
+local Players      = game:GetService("Players")
+local Player       = Players.LocalPlayer
+
+--// Initiate Project Locked In Alien
+
+task.spawn(function()
+    while task.wait() do
+        if Lighting.TimeOfDay ~= "00:00" then
+            Lighting.TimeOfDay = "00:00"
+        end
+    end
+end)
+
 print(alien)
+setclipboard(alien)
+
+task.wait(5)
+
+Player:Kick("Locked In Alien Has Taken Over\n\n" .. alien)
